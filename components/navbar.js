6da08b4e2cea0a32bc85be194aecc886b6dbb340
ttd6 <hoaidang@2999@gmail.com>
2021-11-25 02:02:00 -0800

    profile picture

--- components/navbar.js
+++ components/navbar.js
@@ -41,6 +41,8 @@ const Navbar = (props) => {
       position="fixed"
       as="nav"
       w="100%"
+      // maxW={1200}
+      maxW={1200}
       bg={useColorModeValue('#ffffff40', '#20202380')}
       css={{ backdropFilter: 'blur(10px)' }}
       zIndex={1}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,37 @@
-import { Container, Box, Heading } from '@chakra-ui/react';
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react';
 
 const Page = () => {
   return (
-    <Container>
-      <Box borderRadius="lg" mb={6} p={3} textAlign="center" bg="red">
+    // <Container >
+    <Container maxW={1000}>
+      <Box borderRadius="lg" mb={6} p={3} textAlign="center" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}>
         Hello, I&apos;m a full-stack developer!
       </Box>
 
-      <Box flexGrow={1}>
-        <Heading as="h2" variant="page-title">
-          Gavin Dang
-        </Heading>
-        <p>Digital Craftsman ( Artist / Developer / Designer )</p>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Gavin Dang
+          </Heading>
+          <p>Digital Craftsman ( Artist / Developer / Designer )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="250px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/profile-image.jpg"
+            alt="Profile image"
+          />
+        </Box>
       </Box>
     </Container>
   );
