4a26c93c5f1b345014348217dda56028223707f9
gavindang2911 <hoaidang2999@gmail.com>
2021-12-03 03:57:44 -0800

    Working project route

---
+++ pages/works.js
@@ -0,0 +1,12 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+
+
+const Works = () => (
+      <Container pt={64}>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
+        </Container>
+)
+
+export default Works
