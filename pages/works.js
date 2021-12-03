18dde735b45939b93eaf0060af92109698ea280d
gavindang2911 <hoaidang2999@gmail.com>
2021-12-03 04:10:12 -0800

    Created project card

--- pages/works.js
+++ pages/works.js
@@ -1,12 +1,30 @@
-import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
-
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react';
+import Section from '../components/section';
+import { WorkGridItem } from '../components/projectBox/grid-item';
+import thumbInkdrop from '../public/images/logo2.jpg';
 
 const Works = () => (
-      <Container pt={64}>
-        <Heading as="h3" fontSize={20} mb={4}>
-          Works
-        </Heading>
-        </Container>
-)
+  <Container pt={64}>
+    <Heading as="h3" fontSize={20} mb={4}>
+      Works
+    </Heading>
+    <SimpleGrid columns={[1, 1, 2]} gap={6}>
+      <Section>
+        <WorkGridItem id="todo list" title="Todo App" thumbnail={thumbInkdrop}>
+          CRUD Todo App allows user create doto
+        </WorkGridItem>
+      </Section>
+      <Section>
+        <WorkGridItem
+          id="list issues"
+          title="Issues Tracker"
+          thumbnail={thumbInkdrop}
+        >
+          Keep track of issues
+        </WorkGridItem>
+      </Section>
+    </SimpleGrid>
+  </Container>
+);
 
-export default Works
+export default Works;
