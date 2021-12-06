3ead4c842f094290f8672b02fed04a47e1c12b01
gavindang2911 <hoaidang2999@gmail.com>
2021-12-05 19:26:33 -0800

    Updated about section

--- components/about/about.js
+++ components/about/about.js
@@ -12,19 +12,36 @@ const About = () => {
     <FadeInWhenVisible>
       <Section delay={0.2}>
         <Heading size="2xl" variant="section-title" pl={70}>
-        <SmallTitleText text="About" />
-
+          <SmallTitleText text="About" />
         </Heading>
         <p className="aboutDetail">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.{' '}
+          My name is Gavin Dang and I am a Software Developer, currently living
+          in Vancouver, BC, Canada.
+          <p>- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -</p>
+          <br />
+          I am pursuing a Bachelor's degree in Computing Science at Simon Fraser
+          University. My primary focus and inspiration for my studies are Web
+          and Mobile Development.
+          <br />
+          <p>
+            I am both driven and self-motivated, and I am constantly
+            experimenting with new technologies and techniques. I am very
+            committed towards my projects, and strive to better myself as a
+            developer. I enjoy being challenged and engaging with projects that
+            require me to work outside my comfort and knowledge set.
+          </p>
+          <br />
+          <p>
+            I am experienced in both front and back-end development, and I am
+            proficient in React, NodeJs, SQL/PostgreSQL, MongoDB, Flutter to
+            name a few. I am also familiar with Java, C/C++, Python ..
+          </p>
+          <br />
+          <p>
+            And I believe that we are always eager to teach and help others to
+            develop something special. We are the ones who can help the
+            computers to take right decisions by creating incredible software{' '}
+          </p>
         </p>
       </Section>
     </FadeInWhenVisible>
