9bb41f14f5b577d32e8b4c9488cf9e58495a8def
gavindang2911 <hoaidang2999@gmail.com>
2022-01-10 02:49:26 -0800

    Added animation into intro text

--- components/nameTitle/nameTitle.js
+++ components/nameTitle/nameTitle.js
@@ -1,28 +1,39 @@
-import { Box, Heading } from '@chakra-ui/layout'
-import React from 'react'
-import Avatar from '../avatar/avatar'
-import Section from '../section'
-import SmallTitleText from './smallTitleText'
+import { Box, Heading } from '@chakra-ui/layout';
+import { useColorModeValue } from '@chakra-ui/react';
+import React from 'react';
+import Typical from 'react-typical';
+import Avatar from '../avatar/avatar';
+import Section from '../section';
 
-function NameTitle() {
-    return (
-        <Section delay={0.2}>
-          <Box
-            display={{ md: 'flex' }}
-            justifyContent="space-evenly"
-            h="80vh"
-            pt={40}
-          >
-            <Box align="center" mt={{ base: 5, md: 36 }}>
-              <Heading variant="page-title" size="4xl">
-                Gavin Dang
-              </Heading>
-              <SmallTitleText text="Software Developer / Designer"></SmallTitleText>
-            </Box>
-            <Avatar />
+const NameTitle = () => {
+  const color = useColorModeValue('#104c91', 'greenyellow');
+  return (
+    <Section>
+      <Box
+        display={{ md: 'flex' }}
+        justifyContent="space-evenly"
+        h="80vh"
+        pt={40}
+      >
+        <Box align="center" mt={{ base: 5, md: 36 }}>
+          <Heading variant="page-title" size="4xl">
+            Gavin Dang
+          </Heading>
+
+          <Box color={color}>
+            <h3>
+              <Typical
+                steps={['Software Developer', 1000, 'Designer', 1000]}
+                wrapper="p"
+                loop={Infinity}
+              />
+            </h3>
           </Box>
-        </Section>
-    )
-}
+        </Box>
+        <Avatar />
+      </Box>
+    </Section>
+  );
+};
 
-export default NameTitle
+export default NameTitle;
